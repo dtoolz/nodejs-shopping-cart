3ed85adb5ef2c289c7cda97d86c62679c4641ff8
donald1623 <donaldaudriel@gmail.com>
2020-02-05 06:46:29 +0100

    added bootstrap and jquery cdn to layout.hbs

--- shopping-cart/routes/index.js
+++ shopping-cart/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Dtoolz Express Shopping Cart' });
 });
 
 module.exports = router;
