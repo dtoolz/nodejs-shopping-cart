2cf62859a55dc953d00547648cc0aff867ccd5c5
donald1623 <donaldaudriel@gmail.com>
2020-02-06 06:20:13 +0100

    added UI using bootstrap 4

--- shopping-cart/routes/index.js
+++ shopping-cart/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Dtoolz Express Shopping Cart' });
+  res.render('store/index', { title: 'Dtoolz Express Shopping Cart' });
 });
 
 module.exports = router;
