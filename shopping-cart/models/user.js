998eaa50db1ae1a337a9a271d22e279ff52ab928
donald1623 <donaldaudriel@gmail.com>
2020-02-08 08:51:42 +0100

    added user signup view,csrf protection

---
+++ shopping-cart/models/user.js
@@ -0,0 +1,12 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var userSchema = new Schema({
+   firstName : {type : String, required: true},
+   lastName : {type : String, required: true},
+   email : {type : String, required: true},
+   password : {type : String, required: true},
+   country : {type : String, required: true}
+});
+
+module.exports = mongoose.model('User', userSchema);
--- shopping-cart/routes/index.js
+++ shopping-cart/routes/index.js
@@ -1,7 +1,11 @@
 var express = require('express');
 var router = express.Router();
 var Product = require('../models/product');
+var csrf = require('csurf');
 
+
+var csrfProtection = csrf();
+router.use(csrfProtection); // protecting all routes with csrf
 /* GET home page. */
 router.get('/', function(req, res, next) {
    Product.find(function(err,docs){
@@ -12,6 +16,16 @@ router.get('/', function(req, res, next) {
      }
     res.render('store/index', { title: 'Dtoolz Express Shopping Cart', products : productGroups });
    });
+});
+
+//get signup page
+router.get('/user/signup', function(req,res,next){
+      res.render('user/signup', {csrfToken : req.csrfToken()});
+});
+
+//make post from signup page
+router.post('/user/signup', function(req,res,next){
+     res.redirect('/');
 });
 
 module.exports = router;
